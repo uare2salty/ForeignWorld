df78fbe708ff8ef6e6f0930d33c5c3a1e0691f02
Jonas Sollmann <jsollmann2@gmail.com>
2020-06-16 11:43:46 +0200

    Initial Commit

---
+++ ForeignWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ForeignWorld
+//
+//  Created by Jonas Sollmann on 16.06.20.
+//  Copyright © 2020 Jonas Sollmann. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
